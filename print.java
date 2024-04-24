befd936b10a9e496f2260c2ad019e6f781764245
helgehrishi <helgehrishi@gmail.com>
2024-04-24 13:41:52 +0530

    cube pattern program

--- print.java
+++ print.java
@@ -1,7 +1,12 @@
 class print{
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        for(int i = 0;i<=5 ; i++){
+            for(int j = 0 ; j<=5 ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 
 }
