55b6af3566b2af72c8e8940ee8df97ff176e497e
helgehrishi <helgehrishi@gmail.com>
2024-04-25 09:13:46 +0530

    changes in bubble sorting added -i to reduce the iteration each time

--- array/bubblesort.java
+++ array/bubblesort.java
@@ -6,7 +6,7 @@ public class bubblesort {
         int[] arr = { 5, 4, 3, 2, 1 };
 
         for(int i = 0 ; i<arr.length ; i++){
-            for(int j = 0 ; j<arr.length-1 ; j++){
+            for(int j = 0 ; j<arr.length-1-i ; j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
